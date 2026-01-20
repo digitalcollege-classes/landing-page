a55524a0f35d3c0e2de2088fdd363a21e7014027
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2026-01-20 20:22:33 -0300

    Novo layout do site

--- site/public/index.php
+++ site/public/index.php
@@ -1,6 +1,3 @@
 <?php
 
-include '../pages/navbar.php';
-include '../pages/header.php';
-include '../pages/content-1.php';
-include '../pages/footer.php';
+require_once '../views/base.php';
---
+++ site/views/schedule.php
@@ -0,0 +1,78 @@
+<div class="container" id="schedule">
+    <div class="section">
+        <div class="row">
+            <div class="col s12 center">
+                <h3 class="font-marron"><strong>Programação</strong></h3>
+                <h5 class="font-marron">E o que vai ter de bom?</h5>
+                <div class="col s12 center card brown lighten-4">
+                    <table class="responsive-table highlight">
+                        <thead>
+                            <tr>
+                                <th><strong>Horário</strong></th>
+                                <th><strong>Atividade</strong></th>
+                                <th><strong>Facilitador</strong></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>08:30</td>
+                                <td>Credenciamento + Café</td>
+                                <td>Abertura</td>
+                            </tr>
+                            <tr>
+                                <td>09:00</td>
+                                <td>Abertura</td>
+                                <td>CDL</td>
+                            </tr>
+                            <tr>
+                                <td>09:30</td>
+                                <td>Palestra 1</td>
+                                <td>Comunidade PHP com Rapadura</td>
+                            </tr>
+                            <tr>
+                                <td>10:20</td>
+                                <td>Palestra 2</td>
+                                <td>Comunidade PHP com Rapadura</td>
+                            </tr>
+                            <tr>
+                                <td>11:10</td>
+                                <td>Palestra 3</td>
+                                <td>Comunidade PHP com Rapadura</td>
+                            </tr>
+                            <tr>
+                                <td>12:00</td>
+                                <td>Intervalo para almoço</td>
+                                <td>Todo mundo!</td>
+                            </tr>
+                            <tr>
+                                <td>13:30</td>
+                                <td>Palestra 4</td>
+                                <td>Comunidade PHP com Rapadura</td>
+                            </tr>
+                            <tr>
+                                <td>14:20</td>
+                                <td>Palestra 5</td>
+                                <td>Bluey</td>
+                            </tr>
+                            <tr>
+                                <td>15:10</td>
+                                <td>Mesa redonda</td>
+                                <td>Representantes da comunidade</td>
+                            </tr>
+                            <tr>
+                                <td>16:00</td>
+                                <td>Encerramento e sorteio</td>
+                                <td>Comunidade PHP com Rapadura</td>
+                            </tr>
+                            <tr>
+                                <td>16:30</td>
+                                <td>PHPizza</td>
+                                <td>Todo mundo!</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
