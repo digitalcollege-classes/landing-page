cf7c42340997e15d959d53f5a32703dbd88d98bf
Thiago Lima de Oliveira <thiago.loliveira@dataprev.gov.br>
2026-02-05 09:59:00 -0300

    Modifica o Abestract Model criando uma funcão estática que retorna a lista de tuplas do banco de dados de acordo com a classe concreta

--- site/app/Model/AbstractModel.php
+++ site/app/Model/AbstractModel.php
@@ -14,9 +14,21 @@ abstract class AbstractModel
         $dbname = 'setup_lp';
 
         return new \PDO(
-            'mysql:host=setup-lp_mysql;dbname=setup_lp',
+            "mysql:host={$host};dbname={$dbname}",
             $user,
             $password
         );
+    }
+
+    abstract static function tableData(): array;
+
+    public static function all(): array
+    {
+        $stmt = self::db()->prepare(
+            'SELECT * FROM ' . static::tableData()[0]
+        );
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, static::tableData()[1]);
     }
 }
