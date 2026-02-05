309f2a8e19ff9a557a63630cfa11e842bc5bd1bc
Marcos Vinicio Lima <107953683+viniciolimadev@users.noreply.github.com>
2026-02-05 18:42:54 -0300

    Refatoração dos Models aplicando o princípio DRY (#29)
    * Criando restante das tabelas e corrindo view de list
    
    * Aplicando o principio de dry nos models
    
    ---------
    
    Co-authored-by: Vinicio lima <vinicio.lima@al.ce.gov.br>

--- db.sql
+++ db.sql
@@ -12,3 +12,24 @@ INSERT INTO `tb_alunos`
         (1,'Francisquinha','Rua Vicente Leite, 1010'),
         (2,'Chiquinho','Rua dos Tabajaras, 123')
 ;
+
+DROP TABLE IF EXISTS `palestrante`;
+
+CREATE TABLE IF NOT EXISTS `palestrante` (
+     `id` int NOT NULL AUTO_INCREMENT,
+     `nome` varchar(100) NOT NULL,
+     `email` varchar(255) NOT NULL,
+     `especialidade` varchar(255) NOT NULL,
+     PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `palestra`;
+
+CREATE TABLE IF NOT EXISTS `palestra` (
+     `id` int NOT NULL AUTO_INCREMENT,
+     `titulo` varchar(255) NOT NULL,
+     `palestrante` varchar(100) NOT NULL,
+     `descricao` text NOT NULL,
+     `horario` varchar(50) NOT NULL,
+     PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
