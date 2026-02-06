9604c3fb0edb6860638fef95522c80c5d3a942d3
Vinicio lima <vinicio.lima@al.ce.gov.br>
2026-02-06 10:49:46 -0300

    Adicionando validação de variaveis do dotenv

--- site/app/Connection/DatabaseConnection.php
+++ site/app/Connection/DatabaseConnection.php
@@ -13,6 +13,21 @@ class DatabaseConnection
         $dotenv = new Dotenv();
         $dotenv->load(__DIR__.'/../../.env');
 
+        $required = ['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASSWORD'];
+        $missing = [];
+
+        foreach ($required as $var) {
+            if (!isset($_ENV[$var]) || $_ENV[$var] === '') {
+                $missing[] = $var;
+            }
+        }
+
+        if (!empty($missing)) {
+            throw new \RuntimeException(
+                'Variáveis de ambiente obrigatórias não definidas: ' . implode(', ', $missing)
+            );
+        }
+
         $host = $_ENV['DB_HOST'];
         $dbname = $_ENV['DB_NAME'];
         $user = $_ENV['DB_USER'];
