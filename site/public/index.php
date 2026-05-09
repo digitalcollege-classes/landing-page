bd149396103494593cdf0e922e4441d9a29bdab0
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2026-05-09 08:46:09 -0300

    escondendo sit

--- site/public/index.php
+++ site/public/index.php
@@ -4,10 +4,10 @@ $url = $_SERVER['REQUEST_URI'];
 
 include "../vendor/autoload.php";
 
-if (str_contains($url, '/admin')) {
+//if (str_contains($url, '/admin')) {
     include '../app/routes.php';
 
     exit;
-}
+//}
 
 require_once '../views/base.php';
